0098214bf5f1d4f636a48c4b5157876ca6d1bd47
Ilya Strukov <iley@iley.ru>
2018-02-22 05:10:47 +0100

    Add a cache to RatingClient

--- rating_bot/rating_client.py
+++ rating_bot/rating_client.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+from expiringdict import ExpiringDict
 from .exc import RatingBotError
 from .data_types import Rating
 
@@ -11,7 +12,7 @@ class RatingClient:
     BASE_URL = 'http://rating.chgk.info'
 
     def __init__(self):
-        pass
+        self._cache = ExpiringDict(max_len=100, max_age_seconds=5*60)
 
     def team_info(self, team_id):
         try:
@@ -24,6 +25,9 @@ class RatingClient:
             raise RatingBotError('Команда #%d не найдена: %s' % (team_id, ex))
 
     def get_rating(self, team_id):
+        if team_id in self._cache:
+            log.info('Rating for %d cached' % team_id)
+            return self._cache[team_id]
         try:
             log.info('Fetching rating for %d' % team_id)
             r = requests.get('%s/api/teams/%d/rating.json' % (self.BASE_URL, team_id))
@@ -33,7 +37,9 @@ class RatingClient:
             if not records:
                 raise RatingBotError('Рейтинг для команды #%d не найден' % team_id)
             last_record = max(records, key=lambda d: int(d['idrelease']))
-            return Rating.fromJSON(last_record)
+            rating = Rating.fromJSON(last_record)
+            self._cache[team_id] = rating
+            return rating
 
         except Exception as ex:
             log.exception(ex)
